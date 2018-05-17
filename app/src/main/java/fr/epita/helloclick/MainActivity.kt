dab51150e2f2734d34e6f9cd7bdf72684ad9b388
Joshua <joshua.dandou@gmail.com>
2018-05-17 22:24:39 +0200

    Add: function on click

--- app/src/main/java/fr/epita/helloclick/MainActivity.kt
+++ app/src/main/java/fr/epita/helloclick/MainActivity.kt
@@ -5,17 +5,23 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     var cpt: Int = 0
+    var If_Increas: Int = 0
+    var increasment_size: Int = 1
 
 
     fun sum(a: Int, b: Int): Int {
         return a + b
     }
 
+    val toast = Toast.makeText( this@MainActivity,
+            "you increase counter + 1 !",
+            Toast.LENGTH_SHORT)
     //class MyCustomGestureClass(val name: String?) : Gesture(), View.OnClickListener {
 
     //}
@@ -32,8 +38,36 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         if (clickedView != null) {
             when (clickedView.id) {
                 R.id.CLICK_ME -> {
-                    Log.d("MainActivity", "Main button was clicked !")
-                    cpt += 1
+                    Log.d("MainActivity", "Click me button was clicked !")
+                    if (If_Increas == 0) {
+                        cpt += increasment_size
+                    }
+                    else {
+                        cpt -= increasment_size
+                    }
+                    toast.show()
+                }
+                R.id.RESET -> {
+                    Log.d("MainActivity", "Reset button was clicked !")
+                    cpt = 0
+                }
+                R.id.REVERSE -> {
+                    Log.d("MainActivity", "reverse button was clicked !")
+                    If_Increas = if (If_Increas == 0) {
+                        1
+                    } else {
+                        0
+                    }
+                }
+
+                R.id.BIG -> {
+                    Log.d("MainActivity", "Big button was clicked !")
+                    increasment_size = 10
+                }
+
+                R.id.NORMAL -> {
+                    Log.d("MainActivity", "Reset button was clicked !")
+                    increasment_size = 1
                 }
             }
         }
